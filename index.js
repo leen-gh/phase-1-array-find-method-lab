809c51bb4ea4cdf3c09f86dea83363621c0b4d80
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-13 04:31:09 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const newArgy =[
+    { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+let winYear 
+function superbowlWin(argy) {
+    const h = argy.find((arg)=>{
+        if(arg.result == "W")
+            return arg.year
+        })
+        if (h)
+            return h.year 
+    
+    
+}
